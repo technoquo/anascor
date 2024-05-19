3d5fd2285e83c86bfd0be24cde9924ba509592bc
Leonel Lopez <technoquo@gmail.com>
2024-05-19 12:03:11 +0200

    poner texto siganos en vez de follow us

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -22,7 +22,8 @@
                     </ul>
                 </div>
                 <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                        Síganos</h2>
                     <ul class="text-gray-500 dark:text-gray-400 font-medium">
                         <li class="mb-4">
                             <a href="https://www.facebook.com/anascor74/" target="_blank"
