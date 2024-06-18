df6e6655cdf5efd3e1a7a5ea13d4d4a6c0ebe8bf
Leonel Lopez <technoquo@gmail.com>
2024-06-18 10:58:58 +0200

    agregar SEO

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,21 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+        content="Promueve los derechos de personas sordas, sordociegas y con pérdida auditiva mediante cooperación, campañas, empleo y otros mecanismos con respeto y comprensión.">
+    <meta name="keywords"
+        content="derecho, sordos, LESCO, igualdad, accesible, cultura sorda, lengua de señas, comites, educación">
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('img/anascor.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('img/anascor.ico') }}" type="image/x-icon">
+    <!-- Open Graph Meta Tags -->
+    <meta property="og:title" content="Asociación Nacional de Sordos de Costa Rica">
+    <meta property="og:description"
+        content="Promueve los derechos de personas sordas, sordociegas y con pérdida auditiva mediante cooperación, campañas, empleo y otros mecanismos con respeto y comprensión.">
+    <meta property="og:image" content="https://anascor.org/img/anascor.png">
+    <meta property="og:url" content="https://anascor.org/">
+    <meta property="og:type" content="website">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>@yield('title')</title>
     <script>
@@ -17,6 +32,24 @@
         }
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-TP326ZQ9');
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body class="dark:bg-gray-900">
