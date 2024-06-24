132b77aa32113211ce1375e8eb76ec56c491f44a
Leonel Lopez <technoquo@gmail.com>
2024-06-24 14:20:40 +0200

    cambio diseno de valores con cuadros

--- resources/views/valores.blade.php
+++ resources/views/valores.blade.php
@@ -4,28 +4,132 @@
             <h1 class="font-bold text-3xl dark:text-white">NUESTROS VALORES</h1>
         </div>
         <div class="mx-auto max-w-lg justify-center text-lg dark:text-white mt-10">
-
             <iframe class="w-full aspect-video hover:aspect-square" width="560" height="315"
                 src="https://www.youtube.com/embed/ZZ_CuVy8NEE?si=6a7LUHzc8xlN30u8&amp;controls=0"
                 title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+        <div class="flex flex-row justify-center mx-auto p-4 w-full">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-3 mt-10 text-center items-center">
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
-            <ul class="list-disc list-inside text-gray-500 dark:text-gray-400 mt-9 space-y-2">
-                <li>Compromiso con las personas sordas.</li>
-                <li>Dignidad.</li>
-                <li>Responsabilidad.</li>
-                <li>Honestidad.</li>
-                <li>Transparencia.</li>
-                <li>Compromiso.</li>
-                <li>Excelencia.</li>
-                <li>Respeto.</li>
-                <li>Empatía.</li>
-                <li>Trabajo.</li>
-                <li>Disciplina.</li>
-                <li>Solidaridad.</li>
-                <li>Tolerancia.</li>
-            </ul>
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">Compromiso con las
+                            personas sordas
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Dignidad
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Responsabilidad
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Honestidad
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Transparencia
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Compromiso
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Excelencia
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Respeto
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Empatía
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Trabajo
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Disciplina
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Solidaridad
+                        </h5>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="p-5">
+                        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                            Tolerancia
+                        </h5>
+                    </div>
+                </div>
+            </div>
         </div>
         <x-boton />
     @endsection
