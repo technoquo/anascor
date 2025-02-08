9c5cdb15bb6bdec2935375ff5e584cb08f98a440
Leonel Lopez <technoquo@gmail.com>
2025-02-08 21:05:02 +0100

    agregado logo de singbidger

--- resources/views/menus/convenios.blade.php
+++ resources/views/menus/convenios.blade.php
@@ -4,24 +4,29 @@
             <h1 class="font-bold text-3xl dark:text-white">Convenio con las empresas de tecnologia accesible para las
                 personas sordas</h1>
         </div>
-        <div class="mx-auto max-w-7xl justify-center text-lg dark:text-white mt-10">
-            <div class="md:flex justify-center gap-4">
-                <div class="mx-4">
+        <section class="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-screen-lg  sm:py-16 lg:px-6 justify-center">
+               
+                <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+                   
                     <a href="https://www.helenai.com/" target="_blank">
-                        <img class="h-auto max-w-full rounded-lg"
-                            src="https://media.licdn.com/dms/image/C4E0BAQF8zSaJsFLwhA/company-logo_200_200/0/1673752994949/helenai_logo?e=2147483647&v=beta&t=AJU-kWVfO5bzMlCYGPYDZMfw81o_FpLT5kF2-BJW-Bg"
+                        <img class="h-auto md:w-full w-1/2 rounded-lg"
+                            src="{{ asset('img/helenai_logo.jpg') }}"
                             alt="">
                     </a>
-                </div>
-                <div class="mx-4">
-                    <a href="https://www.sign-bridge.com/" target="_blank">
-                        <img class="h-auto max-w-full rounded-lg"
-                            src="https://media.licdn.com/dms/image/D4E0BAQGMA9ncaD9-bA/company-logo_200_200/0/1689779054821/green_technological_logo?e=1723680000&v=beta&t=1jfc2ym8lZ2Xh3QTvsAVVTJWI03ozphluSt8Bdi2Irc"
-                            alt="">
-                    </a>
+                    <div>
+                        <a href="https://www.sign-bridge.com/" target="_blank">
+                            <img class="md:w-full w-1/2 h-auto rounded-lg"
+                                src="{{ asset('img/signbridge.jpg') }}"
+                                alt="">
+                        </a>
+                    </div>
+                  
                 </div>
             </div>
-
-        </div>
+          </section>
+       
+          
+       
     @endsection
 </x-layout>
